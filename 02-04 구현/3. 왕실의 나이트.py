b486b434e63e3a98a9231b191036e106e75b5b2b
Sungho95 <tjdgh1785@gmail.com>
2022-03-30 21:59:57 +0900

    2022-03-30 update

--- 02-04 구현/3. 왕실의 나이트.py
+++ 02-04 구현/3. 왕실의 나이트.py
@@ -1,9 +1,16 @@
-n = int(input())
-count = 0
-coin = [500, 100, 50, 10]
+input_data = input()
 
-for i in coin:
-    count += n // i
-    n %= i
+col = int(input_data[1])
+row = int(ord(input_data[0])) - 96
+
+# 8가지 경우의 수 (x, y)
+steps = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+
+count = 0
+for i in steps:
+    nc = col + i[0]
+    nr = row + i[1]
+    if 1 <= nc <= 8 and 1 <= nr <= 8:
+        count += 1
 
 print(count)
--- 02-04 구현/4. 게임 개발.py
+++ 02-04 구현/4. 게임 개발.py
@@ -1,9 +1,46 @@
-n = int(input())
-count = 0
-coin = [500, 100, 50, 10]
+n, m = map(int, input().split())
+# 캐릭터의 x, y 좌표와 방향(0: 북, 1: 동, 2: 남, 3: 서)
+x, y, direction = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-for i in coin:
-    count += n // i
-    n %= i
+# 방문 정보
+visited = [[0] * m for _ in range(n)]
+# 초기 위치 방문처리
+visited[x][y] = 1
+
+# 북, 동, 남, 서
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if visited[nx][ny] == 0 and arr[nx][ny] == 0:
+        visited[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn = 0
+        continue
+    else:
+        turn += 1
+    if turn == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if arr[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn = 0
 
 print(count)
